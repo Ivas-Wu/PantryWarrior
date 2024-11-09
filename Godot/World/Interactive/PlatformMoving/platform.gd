55b6153e897c8a4cb33619f085758c88115a6bc6
Ivas Wu <ivas.wu@yahoo.ca>
2024-11-08 23:25:44 -0500

    stage 2 continuation

--- Godot/World/Interactive/PlatformMoving/platform.gd
+++ Godot/World/Interactive/PlatformMoving/platform.gd
@@ -3,15 +3,27 @@ extends Path2D
 @export var loop : bool = true
 @export var speed : int = 1
 @export var speed_scale : float = 1.0
+@export var delay : int = 0
 
 @onready var path = $PathFollow2D
 @onready var animation_player = $AnimationPlayer
+@onready var delay_timer = $Delay
 
 func _ready():
+	set_process(false)
+	if delay > 0:
+		delay_timer.wait_time = delay
+		delay_timer.start()
+		delay_timer.timeout.connect(init)
+	else:
+		init()
+	
+func init():
 	if not loop:
 		animation_player.play("Move")
 		animation_player.speed_scale = speed_scale
-		set_process(false)
+	else:
+		set_process(true)
 
 func _process(_delta):
 	path.progress += speed
